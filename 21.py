5e56fe2602704ce8067b5deec49cd6bb22285cc2
levinhhan <pitou.psy@gmail.com>
2026-01-02 17:57:38 +0700

    21 - Merge Two Sorted Lists

---
+++ 21.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(l1, l2):
+        d = ListNode(0) # dummy node
+        c = d # current node
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                c.next = l1
+                l1 = l1.next
+            else:
+                c.next = l2
+                l2 = l2.next
+            c = c.next
+
+        c.next = l1 if l1 else l2
+
+        return d.next
