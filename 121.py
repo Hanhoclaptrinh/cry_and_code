685eaadeeedf73c183fb936709d21ba6ffc0ed49
levinhhan <pitou.psy@gmail.com>
2026-01-04 15:50:18 +0700

    121 - Best Time to Buy and Sell Stock

---
+++ 121.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(prices) -> int:
+        minp=float('inf') # khoi tao gia mua thap nhat la vo cung
+        maxp=0
+
+        for p in prices:
+            if p < minp:
+                minp=p # cap nhat gia mua thap nhat neu co gia thap hon
+            elif p-minp>maxp:
+                maxp=p-minp # tinh loi nhuan neu ban gia hien tai
+        return maxp
+        
