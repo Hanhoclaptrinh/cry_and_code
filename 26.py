3b1566e210c3af4781c788eff0114ae98d3bcd0b
levinhhan <pitou.psy@gmail.com>
2026-01-07 10:16:27 +0700

    26. Remove Duplicates from Sorted Array

---
+++ 26.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(nums):
+        if not nums: return 0
+        p=1
+        for i in range(1, len(nums)):
+            if nums[i]!=nums[i-1]:
+                nums[p]=nums[i]
+                p+=1
+        return p
