ed9abbbe7a525d80812384ad562bfd08ea59bfe0
vinhhan <pitou.psy@gmail.com>
2026-01-07 21:55:00 +0700

    58. Length of Last Word

---
+++ 58.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(s):
+        l=0
+        i=len(s)-1
+
+        while i>=0 and s[i]=' ':
+            i-=1
+
+        while i>=0 and s[i]!=' ':
+            l+=1
+            i-=1
+        return l
