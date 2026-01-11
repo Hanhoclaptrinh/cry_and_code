c9cf058072d0f49193036871983ebd8cc3349eab
vinhhan <pitou.psy@gmail.com>
2026-01-11 17:45:03 +0700

    number line jump

---
+++ hackerrank/NumberLineJump.py
@@ -0,0 +1,10 @@
+def kangaroo(x1, v1, x2, v2):
+    if v1 == v2:
+        return 'YES' if x1 == x2 else 'NO'
+    
+    n = (x2 - x1) / (v1 - v2)
+    
+    if n >= 0 and n == int(n):
+        return 'YES'
+    
+    return 'NO'
